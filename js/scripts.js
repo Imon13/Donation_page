786f8d5685aa36faf72c35a6a17135fe9a3d233e
imon <imonsaha153@gmail.com>
2025-04-22 18:48:53 +0600

    inintital calculation of cards  has been ompleted

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 const noaBtn = document.getElementById('noa-btn').addEventListener('click', function (e) {
-    console.log('click')
-    console.log(e.target.parentNode.childNodes[7].value);
-    const inputValue = parseInt(e.target.parentNode.childNodes[7].value)
-    console.log(typeof inputValue);
-    const noaFinal = parseInt(document.getElementById('noa-ini').innerText)
-    console.log(typeof noaFinal);
-    const noaDonation = inputValue + noaFinal
-    document.getElementById('noa-ini').innerText = noaDonation
-    const mainDoller = parseInt(document.getElementById('main-doller').innerText)
-    console.log(mainDoller)
-    const afterDonate = mainDoller - noaDonation
-    document.getElementById('main-doller').innerText = afterDonate
+    const inputValue = parseInt(e.target.parentNode.childNodes[7].value);
+    const noaFinal = parseInt(document.getElementById('noa-ini').innerText);
+    const noaDonation = inputValue + noaFinal;
+    document.getElementById('noa-ini').innerText = noaDonation;
 
+    const mainDoller = parseInt(document.getElementById('main-doller').innerText);
+    const afterDonate = mainDoller - inputValue;
+    document.getElementById('main-doller').innerText = afterDonate;
+});
 
+const quotaBtn = document.getElementById('quota-btn').addEventListener('click', function (e) {
+    const inputValue = parseInt(e.target.parentNode.childNodes[7].value);
+    const quotaFinal = parseInt(document.getElementById('quota').innerText);
+    const quotaDonation = inputValue + quotaFinal;
+    document.getElementById('quota').innerText = quotaDonation;
 
-})
+    const mainDoller = parseInt(document.getElementById('main-doller').innerText);
+    const afterDonatequota = mainDoller - inputValue;
+    document.getElementById('main-doller').innerText = afterDonatequota;
+});
+
+const feniBtn = document.getElementById('feni-btn').addEventListener('click', function (e) {
+    const inputValue = parseInt(e.target.parentNode.childNodes[7].value);
+    const feniFinal = parseInt(document.getElementById('feni').innerText);
+    const feniDonation = inputValue + feniFinal;
+    document.getElementById('feni').innerText = feniDonation;
+
+    const mainDoller = parseInt(document.getElementById('main-doller').innerText);
+    const afterDonatefeni = mainDoller - inputValue;
+    document.getElementById('main-doller').innerText = afterDonatefeni;
+});
